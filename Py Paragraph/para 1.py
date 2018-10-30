a85f7dce61d8dd8e8cdfe34b5636b3eeac4f621b
PurnimaChandel <43767250+PurnimaChandel@users.noreply.github.com>
2018-10-30 18:41:14 -0400

    Python Homework

---
+++ Py Paragraph/para 1.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+f = open('paragraph_1.txt', 'r')
+content = f.read()
+
+words = re.split(r"[\s\.,\?]+",content)
+characters = 0
+
+
+d = len(words)
+
+sentence = content.count('.')
+
+avg_sent_count = d/sentence
+
+for words in content:
+    characters += sum(len(word) for word in words)
+    average_char = characters/d
+     
+print ("Paragraph Analysis")
+print ("-----------------")
+print ("Approximate Word Count: " + str(d))
+print ("Approximate Sentence Count: " + str(sentence))
+print ("Average Sentence Length: " + str(avg_sent_count))
+print ("Average Letter Count: " + str(average_char))
+
