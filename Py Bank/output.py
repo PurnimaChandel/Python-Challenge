b74c20b91e54083b7fd5c31f9bdfc60aeaad134e
PurnimaChandel <43767250+PurnimaChandel@users.noreply.github.com>
2018-11-03 19:38:23 -0400

    Update Py Bank/output.py

---
+++ Py Bank/output.py
@@ -0,0 +1,10 @@
+3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)]
+Python Type "help", "copyright", "credits" or "license" for more information.
+[evaluate py bank final.py]
+Financial Analysis
+----------------------------
+Total Months: 86
+Total: $38382578
+Average Change: $-2315.1176470588234
+Greatest Increase in Profits: Feb-2012 $1926159
+Greatest Decrease in Profits: Sep-2013 $-2196167
